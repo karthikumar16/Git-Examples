31e3c9866b0574b3ad611d6dd38b618f27c743cc
Kasirajan00 <114916428+Kasirajan00@users.noreply.github.com>
2022-10-04 10:33:21 +0530

    update var in c

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ function example()
 {
     var a=10;
     var b=20;
-    c=a+b;
+    var c=a+b;
     console.log("The value of a is"+a);
     console.log("The Example of GIT");
     console.log("The value is"+c);
